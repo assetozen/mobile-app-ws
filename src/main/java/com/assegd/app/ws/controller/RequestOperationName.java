072a8b027ad560d04a7c4e19efe122fa37f53d6e
assegd <assetozen@gmail.com>
2020-04-22 17:41:17 +0200

    newportitlaptop usercontroller changes * changes made after that 24.4.2020

---
+++ src/main/java/com/assegd/app/ws/controller/RequestOperationName.java
@@ -0,0 +1,6 @@
+package com.assegd.app.ws.controller;
+
+public enum RequestOperationName {
+
+    DELETE, VERIFY_EMAIL;
+}
--- src/main/java/com/assegd/app/ws/shared/Utils.java
+++ src/main/java/com/assegd/app/ws/shared/Utils.java
@@ -1,8 +1,13 @@
 package com.assegd.app.ws.shared;
 
+import com.assegd.app.ws.security.SecurityConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
 
 import java.security.SecureRandom;
+import java.util.Date;
 import java.util.Random;
 
 @Component
@@ -24,6 +29,26 @@ public class Utils {
             returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
         }
         return new String(returnValue);
+    }
+
+    public static boolean hasTokenExpired(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.getTokenSecret())
+                .parseClaimsJws(token).getBody();
+        Date tokenExpirationDate = claims.getExpiration();
+        Date todyDate = new Date();
+
+        return tokenExpirationDate.before((todyDate));
+    }
+
+    public String generateEmailVerificationToken(String userId)
+    {
+        String token = Jwts.builder()
+                .setSubject(userId)
+                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.getTokenSecret())
+                .compact();
+        return token;
     }
 
 }
